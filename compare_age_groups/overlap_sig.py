2c35413aa37699f79a681b1f9ccd91887cf00e3a
Patrick <patrick.bryant@live.com>
2020-09-03 15:43:00 +0200

    value plots

--- compare_age_groups/overlap_sig.py
+++ compare_age_groups/overlap_sig.py
@@ -32,19 +32,56 @@ parser.add_argument('--outdir', nargs=1, type= str, default=sys.stdin, help = 'P
 
 
 #######FUNCTIONS#######
-def adjust_pvals(corr_df, id):
+def adjust_pvals(corr_df):
     '''Adjust the pvalues
     '''
     res = multipletests(corr_df['p'], 0.05)
     rej, cor_pval = res[0], res[1]
     corr_df['Rejection on 0.05']=rej
     corr_df['qval']=cor_pval
-    print(rej[rej==True].shape[0], 'out of', len(corr_df))
+    num_sig = rej[rej==True].shape[0]
+    print(num_sig, 'out of', len(corr_df))
 
 
-    return corr_df[corr_df['Rejection on 0.05']==True]
+    return corr_df, num_sig
 
+#corr_df[corr_df['Rejection on 0.05']==True]
 
+def plot_qvals(qvals, ai, bi, outdir):
+    #Plot pvalue distribution
+    agebins = ['19-30','30-40','40-50','50-60','60-70','70-80','80+']
+    fig,ax = plt.subplots(figsize=(4.5/2.54, 4.5/2.54))
+    sns.distplot(qvals)
+    plt.title(agebins[ai]+' vs '+agebins[bi])
+    plt.xlabel('q-value')
+    plt.tight_layout()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.savefig(outdir+'qval_'+str(ai)+'_'+str(bi)+'.png', format='png', dpi=300)
+    plt.close()
+
+def volcano_plot(fold_change, qvals, ai, bi, outdir):
+    '''Do a volcano plot
+    '''
+    agebins = ['19-30','30-40','40-50','50-60','60-70','70-80','80+']
+    fig,ax = plt.subplots(figsize=(4.5/2.54, 4.5/2.54))
+    log2fc = np.log2(fold_change)
+    neglog10qval = -np.log10(qvals)
+    plt.scatter(log2fc,neglog10qval, s=0.2, color='lightsteelblue')
+    t  =-np.log10(0.05)
+    plt.axhline(t, min(log2fc),max(log2fc), linewidth=0.3, linestyle ="--", color = 'firebrick', label='bonferroni threshold')
+    #Plot those with fold change less than 2 (log2=1) (or half = -1)
+    fc_t = np.log2(1.5)
+    high_fc_i = np.where((log2fc<-fc_t)|(log2fc>fc_t))
+    plt.scatter(log2fc[high_fc_i[0]],neglog10qval[high_fc_i[0]], s=0.2, color='midnightblue', label='FC>1.5')
+    plt.title(agebins[ai]+' vs '+agebins[bi])
+    plt.xlabel('log2 fold change')
+    plt.ylabel('-log10 p-value')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.tight_layout()
+    plt.savefig(outdir+'qval_volcano_'+str(ai)+'_'+str(bi)+'.png', format='png', dpi=300)
+    plt.close()
 
 ###########MAIN###########
 #Plt
@@ -63,28 +100,22 @@ outdir = args.outdir[0]
 #Get age group correlations
 
 joined_dfs = pd.DataFrame()
-ids = []
+ids1 = []
+ids2 = []
 num_sig = []
-for i in range(6):
-    corr_df = pd.read_csv(indir+str(i)+'_corr_results.csv')
+for i in range(7):
+    for j in range(i+1,7):
+        corr_df = pd.read_csv(indir+str(i)+'_'+str(j)+'_corr_results.csv')
+        #Adjust pvals
+        adjusted_corr_df,sig = adjust_pvals(corr_df)
+        adjusted_corr_df['id1']=i
+        adjusted_corr_df['id2']=j
+        #Volcano plot
+        volcano_plot(np.array(adjusted_corr_df['fold_change']), np.array(adjusted_corr_df['qval']), i, j, outdir)
+        #qval plot
+        plot_qvals(np.array(adjusted_corr_df['p']), i, j, outdir)
+        num_sig.append(sig)
+        #adjusted_corr_df['id']=id
+        joined_dfs = joined_dfs.append(adjusted_corr_df)
 
-    id = str(i)
-    #Adjust pvals
-    adjusted_corr_df = adjust_pvals(corr_df, id)
-    ids.append(int(id))
-    num_sig.append(len(adjusted_corr_df))
-    #adjusted_corr_df['id']=id
-    joined_dfs = joined_dfs.append(adjusted_corr_df)
-
-
-#Plot num significant against grouping
-age_groups = np.load(indir+'age_groups.npy', allow_pickle=True)
-agebins = ['19-30 vs 30-40','30-40 vs 40-50','40-50 vs 50-60','50-60 vs 60-70','60-70 vs 70-80','70-80 vs 80+']
-fig,ax = plt.subplots(figsize=(12/2.54, 12/2.54))
-plt.bar(ids,num_sig)
-plt.xticks(ids,agebins, rotation='vertical')
-plt.ylabel('Number significant markers')
-plt.title('Significant markers (FDR 0.05)')
-plt.tight_layout()
-plt.savefig(outdir+'num_sig_agegroups.png', format = 'png', dpi=300)
 pdb.set_trace()
--- results/montage.sh
+++ results/montage.sh
@@ -8,3 +8,16 @@ montage volcano_0_5.png volcano_1_5.png volcano_2_5.png volcano_3_5.png volcano_
 montage volcano_0_6.png volcano_1_6.png volcano_2_6.png volcano_3_6.png volcano_4_6.png volcano_5_6.png whitebox.png -tile 7x1 -geometry +2+2 row7.png
 #All rows together
 montage row1.png row2.png row3.png row4.png row5.png row6.png row7.png -tile 1x7 -geometry +2+2 matrix.png
+
+
+#qval volcano
+#Montage in a 7x7 grid having the qvalue distributions on the uppertriangular and qval_volcano on lower
+montage whitebox_45.png qval_0_1.png qval_0_2.png qval_0_3.png qval_0_4.png qval_0_5.png qval_0_6.png -tile 7x1 -geometry +2+2 row1.png
+montage qval_volcano_0_1.png whitebox_45.png qval_1_2.png qval_1_3.png qval_1_4.png qval_1_5.png qval_1_6.png -tile 7x1 -geometry +2+2 row2.png
+montage qval_volcano_0_2.png qval_volcano_1_2.png whitebox_45.png qval_2_3.png qval_2_4.png qval_2_5.png qval_2_6.png -tile 7x1 -geometry +2+2 row3.png
+montage qval_volcano_0_3.png qval_volcano_1_3.png qval_volcano_2_3.png whitebox_45.png qval_3_4.png qval_3_5.png qval_3_6.png -tile 7x1 -geometry +2+2 row4.png
+montage qval_volcano_0_4.png qval_volcano_1_4.png qval_volcano_2_4.png qval_volcano_3_4.png whitebox_45.png qval_4_5.png qval_4_6.png -tile 7x1 -geometry +2+2 row5.png
+montage qval_volcano_0_5.png qval_volcano_1_5.png qval_volcano_2_5.png qval_volcano_3_5.png qval_volcano_4_5.png whitebox_45.png qval_5_6.png -tile 7x1 -geometry +2+2 row6.png
+montage qval_volcano_0_6.png qval_volcano_1_6.png qval_volcano_2_6.png qval_volcano_3_6.png qval_volcano_4_6.png qval_volcano_5_6.png whitebox_45.png -tile 7x1 -geometry +2+2 row7.png
+#All rows together
+montage row1.png row2.png row3.png row4.png row5.png row6.png row7.png -tile 1x7 -geometry +2+2 qval_matrix.png
