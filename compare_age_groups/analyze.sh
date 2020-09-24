13c1909a56406c942e98c4a8b9e431edfee4d981
Patrick <pbryant@ae18a.scilifelab.se>
2020-09-24 12:15:55 +0200

    plotting marker values vs genes

--- compare_age_groups/analyze.sh
+++ compare_age_groups/analyze.sh
@@ -11,4 +11,4 @@ INDIR=../results/
 #./overlap_sig.py --gene_annotations $ANNO --joined_betas $BETAS --sample_sheet $SAMPLES --agelabel $AL --indir $INDIR --outdir $OUTDIR
 
 #Visualize selected markers
-#./vis_selected_markers.py --gene_annotations $ANNO --joined_betas $BETAS --sample_sheet $SAMPLES --agelabel $AL --outdir $OUTDIR
+./vis_selected_markers.py --gene_annotations $ANNO --joined_betas $BETAS --sample_sheet $SAMPLES --agelabel $AL --overlapping_probes ../results/overlapping_probes_df.csv --overlapping_genes ../results/total_gene_df.csv --outdir $OUTDIR
--- compare_age_groups/vis_selected_markers.py
+++ compare_age_groups/vis_selected_markers.py
@@ -25,6 +25,8 @@ parser.add_argument('--joined_betas', nargs=1, type= str, default=sys.stdin, hel
 parser.add_argument('--sample_sheet', nargs=1, type= str, default=sys.stdin, help = 'Path to sample sheet.')
 parser.add_argument('--gene_annotations', nargs=1, type= str, default=sys.stdin, help = 'Path to gene annotations.')
 parser.add_argument('--agelabel', nargs=1, type= str, default=sys.stdin, help = 'Agelabel.')
+parser.add_argument('--overlapping_probes', nargs=1, type= str, default=sys.stdin, help = 'Path to overlapping probes.')
+parser.add_argument('--overlapping_genes', nargs=1, type= str, default=sys.stdin, help = 'Path to overlapping probes.')
 parser.add_argument('--outdir', nargs=1, type= str, default=sys.stdin, help = 'Path to outdir.')
 
 
@@ -47,6 +49,16 @@ def get_ages(sample_sheet, sample_names, agelabel):
 
     return np.array(sample_ages)
 
+def group_by_age(ages):
+
+    age_indices = []
+    bins = [19,30,40,50,60,70,80,max(ages)+1]
+    for i in range(len(bins)-1):
+        sel = np.where((ages>=bins[i])&(ages<bins[i+1]))
+        age_indices.append(sel[0])
+
+    return age_indices
+
 
 def format_probes(joined_betas, sample_sheet, gene_annotations, outdir):
     '''Format the probes, their values and the sample ages
@@ -60,16 +72,14 @@ def format_probes(joined_betas, sample_sheet, gene_annotations, outdir):
     zero_indices = np.where(zeros<10)
     #Get ages
     ages = get_ages(sample_sheet, joined_betas.columns[2:], agelabel)
+    #Get age groups
+    age_indices = group_by_age(ages)
+
     #Save ages
     age_df = pd.DataFrame()
     age_df['Sample'] = joined_betas.columns[2:]
     age_df['Age'] = ages
     age_df.to_csv(outdir+'ages.csv')
-
-    #Get age groups
-    age_indices = group_by_age(ages)
-    #Save age groups
-    np.save(outdir+'age_groups.npy',np.array(age_indices))
 
     #Looking at single marker comparisons
     markers = np.array(merged['Reporter Identifier'])
@@ -84,8 +94,43 @@ def format_probes(joined_betas, sample_sheet, gene_annotations, outdir):
     X = X[zero_indices,:][0]
 
 
-    return X, markers, ages
+    return X, markers, ages, age_indices
 
+
+def plot_probes(X,markers,ages,age_indices,overlapping_probes):
+    '''Plot the change in probe vs age.
+    '''
+
+    def running_average(ages,age_indices,vals):
+        x = []
+        y = []
+        for i in range(len(age_indices)):
+            x.append(np.average(ages[age_indices[i]]))
+            y.append(np.average(vals[age_indices[i]]))
+
+        return x,y
+    u_probes = overlapping_probes['Reporter Identifier'].unique()
+    for u_probe in u_probes:
+        sel = overlapping_probes[overlapping_probes['Reporter Identifier']==u_probes[0]]
+        sel = sel.reset_index()
+        i = np.where(markers==u_probe)[0]
+        vals = X[i,:][0,:]
+        #Plot ages vs vals
+        fig,ax = plt.subplots(figsize=(6/2.54, 6/2.54))
+        #Get ra
+        x_av,y_av = running_average(ages,age_indices,vals)
+        plt.plot(x_av,y_av, color = 'k', linewidth=1)
+        plt.scatter(ages, vals, color = 'midnightblue', s=1)
+
+        #Format plot
+        plt.title(u_probe)
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        plt.ylabel('Beta value')
+        plt.tight_layout()
+        plt.savefig(outdir+'fold_changes/markers/'+u_probe+'_vs_age.png', format='png', dpi=300)
+        plt.close()
+    pdb.set_trace()
 ###########MAIN###########
 #Plt
 plt.rcParams.update({'font.size': 7})
@@ -99,5 +144,7 @@ print('Read betas')
 sample_sheet = pd.read_csv(args.sample_sheet[0], sep = '\t')
 outdir = args.outdir[0]
 overlapping_probes = pd.read_csv(args.overlapping_probes[0])
+overlapping_genes = pd.read_csv(args.overlapping_genes[0])
 #Get data
-X, markers, ages = format_probes(joined_betas, sample_sheet, gene_annotations, outdir)
+X, markers, ages, age_indices = format_probes(joined_betas, sample_sheet, gene_annotations, outdir)
+plot_probes(X,markers,ages,age_indices,overlapping_probes)
