16a91c6355c90d1ec08459803a651fd95f1413ff
Patrick <pbryant@ae18a.scilifelab.se>
2020-10-14 11:37:23 +0200

    reran cerebellum

--- results/point_average/brain/cerebellum/genes/montage.sh
+++ results/point_average/brain/cerebellum/genes/montage.sh
@@ -1,2 +1,2 @@
 #Figure 4
-montage nestin.png 'cartilage oligomeric matrix protein.png' nestin.png 'NFKB inhibitor zeta.png' 'solute carrier family 5 member 5.png' 'myogenic differentiation 1.png' -tile 3x2 -title 'Cerebellum' -pointsize 30 -geometry +2+2 all.png
+montage nestin.png 'cartilage oligomeric matrix protein.png' 'NFKB inhibitor zeta.png' 'solute carrier family 5 member 5.png' 'myogenic differentiation 1.png' -tile 3x2 -title 'Cerebellum' -pointsize 30 -geometry +2+2 all.png
