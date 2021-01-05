43e97b980440885280b018afb2b5ca2df98cc8aa
Patrick Bryant <>
2021-01-05 17:19:35 +0100

    adding GO terms per cluster

--- point_average/GO.sh
+++ point_average/GO.sh
@@ -2,8 +2,11 @@
 BLOODGO1=../results/point_average/blood/genes/pantherChart_cluster1.txt
 BLOODGO2=../results/point_average/blood/genes/pantherChart_cluster2.txt
 FCTXGO=../results/point_average/brain/frontal_cortex/genes/pantherChart.txt
-CEREBELLUMGO=../results/point_average/brain/cerebellum/genes/all_pantherChart.txt
+CEREBELLUMGO1=../results/point_average/brain/cerebellum/genes/pantherChart_cluster1.txt
+CEREBELLUMGO2=../results/point_average/brain/cerebellum/genes/pantherChart_cluster2.txt
+CEREBELLUMGO3=../results/point_average/brain/cerebellum/genes/pantherChart_cluster3.txt
+CEREBELLUMGO4=../results/point_average/brain/cerebellum/genes/pantherChart_cluster4.txt
 HANNUMGO=../results/point_average/blood/genes/Hannum/pantherChart.txt
 HORVATHGO=../results/point_average/brain/cerebellum/genes/horvath/pantherChart.txt
 OUTDIR=../results/point_average/
-./gene_ontology.py --blood1 $BLOODGO1 --blood2 $BLOODGO2 --frontal_cortex $FCTXGO --cerebellum $CEREBELLUMGO --horvath $HORVATHGO --hannum $HANNUMGO --outdir $OUTDIR
+./gene_ontology.py --blood1 $BLOODGO1 --blood2 $BLOODGO2 --frontal_cortex $FCTXGO --cerebellum1 $CEREBELLUMGO1 --cerebellum2 $CEREBELLUMGO2 --cerebellum3 $CEREBELLUMGO3 --cerebellum4 $CEREBELLUMGO4 --horvath $HORVATHGO --hannum $HANNUMGO --outdir $OUTDIR
--- point_average/gene_ontology.py
+++ point_average/gene_ontology.py
@@ -22,7 +22,10 @@ parser = argparse.ArgumentParser(description = '''Visualize the gene ontlogy''')
 parser.add_argument('--blood1', nargs=1, type= str, default=sys.stdin, help = 'Path to blood GO1.')
 parser.add_argument('--blood2', nargs=1, type= str, default=sys.stdin, help = 'Path to blood GO2.')
 parser.add_argument('--frontal_cortex', nargs=1, type= str, default=sys.stdin, help = 'Path to FCTX GO.')
-parser.add_argument('--cerebellum', nargs=1, type= str, default=sys.stdin, help = 'Path to cerebellum GO.')
+parser.add_argument('--cerebellum1', nargs=1, type= str, default=sys.stdin, help = 'Path to cerebellum GO1.')
+parser.add_argument('--cerebellum2', nargs=1, type= str, default=sys.stdin, help = 'Path to cerebellum GO2.')
+parser.add_argument('--cerebellum3', nargs=1, type= str, default=sys.stdin, help = 'Path to cerebellum GO3.')
+parser.add_argument('--cerebellum4', nargs=1, type= str, default=sys.stdin, help = 'Path to cerebellum GO4.')
 parser.add_argument('--horvath', nargs=1, type= str, default=sys.stdin, help = 'Path to Horvath GO.')
 parser.add_argument('--hannum', nargs=1, type= str, default=sys.stdin, help = 'Path to Hannum GO.')
 parser.add_argument('--outdir', nargs=1, type= str, default=sys.stdin, help = 'Path to outdir.')
@@ -60,24 +63,38 @@ args = parser.parse_args()
 blood1 = pd.read_csv(args.blood1[0],sep='\t', header=None)
 blood2 = pd.read_csv(args.blood2[0],sep='\t', header=None)
 frontal_cortex = pd.read_csv(args.frontal_cortex[0],sep='\t', header=None)
-cerebellum = pd.read_csv(args.cerebellum[0],sep='\t', header=None)
+cerebellum1 = pd.read_csv(args.cerebellum1[0],sep='\t', header=None)
+cerebellum2 = pd.read_csv(args.cerebellum2[0],sep='\t', header=None)
+cerebellum3 = pd.read_csv(args.cerebellum3[0],sep='\t', header=None)
+cerebellum4 = pd.read_csv(args.cerebellum4[0],sep='\t', header=None)
 horvath = pd.read_csv(args.horvath[0],sep='\t', header=None)
 hannum = pd.read_csv(args.hannum[0],sep='\t', header=None)
 outdir = args.outdir[0]
 
 all_categories = np.concatenate([np.array(blood1[1].unique()),np.array(blood2[1].unique()),
                 np.array(frontal_cortex[1].unique()),
-                np.array(cerebellum[1].unique()),np.array(horvath[1].unique()),np.array(hannum[1].unique())])
+                np.array(cerebellum1[1].unique()),
+                np.array(cerebellum2[1].unique()),
+                np.array(cerebellum3[1].unique()),
+                np.array(cerebellum4[1].unique()),
+                np.array(horvath[1].unique()),np.array(hannum[1].unique())])
 all_categories = np.unique(all_categories)
 
 #Plot ra GO
 colors = pl.cm.tab20b(np.linspace(0,1,len(all_categories)))
 
-
+#Blood
 plot_GO(blood1,colors,all_categories,'Blood GO enrichment 1',outdir+'blood/genes/go1.png')
 plot_GO(blood2,colors,all_categories,'Blood GO enrichment 2',outdir+'blood/genes/go2.png')
+#FCTX
 plot_GO(frontal_cortex,colors,all_categories,'Frontal cortex GO enrichment',outdir+'brain/frontal_cortex/genes/go.png')
-plot_GO(cerebellum,colors,all_categories,'Cerebellum GO enrichment',outdir+'brain/cerebellum/genes/go.png')
+#Cerebellum
+plot_GO(cerebellum1,colors,all_categories,'Cerebellum GO enrichment 1',outdir+'brain/cerebellum/genes/go1.png')
+plot_GO(cerebellum2,colors,all_categories,'Cerebellum GO enrichment 2',outdir+'brain/cerebellum/genes/go2.png')
+plot_GO(cerebellum3,colors,all_categories,'Cerebellum GO enrichment 3',outdir+'brain/cerebellum/genes/go3.png')
+plot_GO(cerebellum4,colors,all_categories,'Cerebellum GO enrichment 4',outdir+'brain/cerebellum/genes/go4.png')
 
+#Hannum
 plot_GO(hannum,colors,all_categories,'Hannum GO enrichment', outdir+'blood/genes/Hannum/go.png')
+#Horvath
 plot_GO(horvath,colors,all_categories,'Horvath GO enrichment', outdir+'brain/go.png')
