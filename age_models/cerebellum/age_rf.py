87512442e58fe03196b057ad1367197192b7ea76
Patrick Bryant <>
2021-01-05 09:58:26 +0100

    plotting pred

--- age_models/cerebellum/age_rf.py
+++ age_models/cerebellum/age_rf.py
@@ -26,15 +26,32 @@ parser.add_argument('--ages', nargs=1, type= str, default=sys.stdin, help = 'Pat
 
 parser.add_argument('--outdir', nargs=1, type= str, default=sys.stdin, help = 'Path to outdir.')
 
-def rf_fit(sel_marker_values, ages):
+def rf_fit(sel_marker_values, ages, outdir):
     '''5 fold CV
     '''
 
     kf = KFold(n_splits=5, random_state=42, shuffle=True)
+    fig,ax = plt.subplots(figsize=(6/2.54, 6/2.54))
+    errors = []
     for ti, vi in kf.split(sel_marker_values):
-        X = sel_marker_values[ti]
-        y = ages[ti]
-        pdb.set_trace()
+        X_train = sel_marker_values[ti]
+        y_train = ages[ti]
+        X_valid = sel_marker_values[vi]
+        y_valid = ages[vi]
+        #Define model
+        #All default params are used - no optimization is performed
+        regr = RandomForestRegressor(n_jobs=-1, random_state=42)
+        regr.fit(X_train, y_train)
+        pred = regr.predict(X_valid)
+        errors.append(np.average(np.absolute(pred-y_valid)))
+        plt.scatter(pred,y_valid,s=1,color='cornflowerblue')
+    plt.xlabel('Predicted age')
+    plt.ylabel('True age')
+    plt.title('Average error '+str(np.round(np.average(errors),2))+' +/- '+str(np.round(np.std(errors),2)))
+    plt.tight_layout()
+    plt.savefig(outdir+'cv_results.png', format='png', dpi=300)
+    plt.close()
+
 
 ###########MAIN###########
 #Plt
@@ -51,4 +68,4 @@ sel_marker_values = marker_values[selected_markers['Unnamed: 0_x'].values]
 #Select ages
 ages = age_df['Age'].values
 #Fit a rf model
-rf_fit(sel_marker_values.T, ages)
+rf_fit(sel_marker_values.T, ages, outdir)
