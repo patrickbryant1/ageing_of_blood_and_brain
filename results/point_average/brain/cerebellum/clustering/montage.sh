79711a860c1f21d76fbd5c10f9f53f93d4385ba3
Patrick <pbryant@ae18a.scilifelab.se>
2021-09-22 09:07:26 +0200

    plots

--- results/point_average/brain/cerebellum/clustering/montage.sh
+++ results/point_average/brain/cerebellum/clustering/montage.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env
-convert  1.png -pointsize 60 -gravity NorthWest -annotate +0+0 "A"  1.png
-convert  2.png -pointsize 60 -gravity NorthWest -annotate +0+0 "B"  2.png
-convert  3.png -pointsize 60 -gravity NorthWest -annotate +0+0 "C"  3.png
-convert  4.png -pointsize 60 -gravity NorthWest -annotate +0+0 "D"  4.png
-convert  tsne.png -pointsize 60 -gravity NorthWest -annotate +0+0 "E" tsne.png
-convert gradients.png -pointsize 60 -gravity NorthWest -annotate +0+0 "F" gradients.png
-montage 1.png 2.png 3.png 4.png tsne.png gradients.png -tile 3x2 -geometry +2+2  Figure5.png
+
+#Montage
+for mode in 'abs' 'FC' 'overlap'
+do
+convert  $mode'1.png' -pointsize 60 -gravity NorthWest -annotate +0+0 "A"  $mode'1.png'
+convert  $mode'2.png' -pointsize 60 -gravity NorthWest -annotate +0+0 "B"  $mode'2.png'
+convert  $mode'3.png' -pointsize 60 -gravity NorthWest -annotate +0+0 "C"  $mode'3.png'
+convert  $mode'4.png' -pointsize 60 -gravity NorthWest -annotate +0+0 "D"  $mode'4.png'
+convert  $mode'_tsne.png' -pointsize 60 -gravity NorthWest -annotate +0+0 "E" $mode'_tsne.png'
+convert $mode'_gradients.png' -pointsize 60 -gravity NorthWest -annotate +0+0 "F" $mode'_gradients.png'
+montage $mode'1.png' $mode'2.png' $mode'3.png' $mode'4.png' $mode'_tsne.png' $mode'_gradients.png' -tile 3x2 -geometry +2+2  $mode'_montage.png'
 #-title 'Cerebellum' -pointsize 30
 
 
-convert  1_unnormalized.png -pointsize 60 -gravity NorthWest -annotate +0+0 "A"  1_unnormalized.png
-convert  2_unnormalized.png -pointsize 60 -gravity NorthWest -annotate +0+0 "B"  2_unnormalized.png
-convert  3_unnormalized.png -pointsize 60 -gravity NorthWest -annotate +0+0 "C"  3_unnormalized.png
-convert  4_unnormalized.png -pointsize 60 -gravity NorthWest -annotate +0+0 "D"  4_unnormalized.png
-montage 1_unnormalized.png 2_unnormalized.png 3_unnormalized.png 4_unnormalized.png -tile 2x2 -geometry +2+2  FigureS4.png
+# convert  1_unnormalized.png -pointsize 60 -gravity NorthWest -annotate +0+0 "A"  1_unnormalized.png
+# convert  2_unnormalized.png -pointsize 60 -gravity NorthWest -annotate +0+0 "B"  2_unnormalized.png
+# convert  3_unnormalized.png -pointsize 60 -gravity NorthWest -annotate +0+0 "C"  3_unnormalized.png
+# convert  4_unnormalized.png -pointsize 60 -gravity NorthWest -annotate +0+0 "D"  4_unnormalized.png
+# montage 1_unnormalized.png 2_unnormalized.png 3_unnormalized.png 4_unnormalized.png -tile 2x2 -geometry +2+2  FigureS4.png
+done
