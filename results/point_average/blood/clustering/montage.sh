a67e1bbc535cc0a1c0f032ef97655899f295ce21
Patrick <pbryant@ae18a.scilifelab.se>
2021-12-08 16:13:55 +0100

    montage

--- results/point_average/blood/clustering/montage.sh
+++ results/point_average/blood/clustering/montage.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env
 #Add labels
-convert  1.png -pointsize 60 -gravity NorthWest -annotate +0+0 "A"  1.png
-convert  2.png -pointsize 60 -gravity NorthWest -annotate +0+0 "B"  2.png
-convert  tsne.png -pointsize 60 -gravity NorthWest -annotate +0+0 "C" tsne.png
-convert gradients.png -pointsize 60 -gravity NorthWest -annotate +0+0 "D" gradients.png
-montage 1.png 2.png tsne.png gradients.png -tile 2x2 -geometry +2+2 Figure3.png
-# -title 'Blood' -pointsize 30
+convert FC1.png -pointsize 50 -gravity NorthWest -annotate +0+0 "A" FC1.png
+convert FC2.png -pointsize 50 -gravity NorthWest -annotate +0+0 "B" FC2.png
+convert FC_tsne.png -pointsize 50 -gravity NorthWest -annotate +0+0 "C" FC_tsne.png
+convert FC_gradients.png -pointsize 50 -gravity NorthWest -annotate +0+0 "D" FC_gradients.png
+montage FC1.png FC2.png FC_tsne.png FC_gradients.png -title 'FC' -pointsize 30 -tile 3x2 -geometry +2+2 FC_montage.png
+#Add labels
+convert abs1.png -pointsize 50 -gravity NorthWest -annotate +0+0 "E" abs1.png
+convert abs_tsne.png -pointsize 50 -gravity NorthWest -annotate +0+0 "F" abs_tsne.png
+convert abs_gradients.png -pointsize 50 -gravity NorthWest -annotate +0+0 "G" abs_gradients.png
+montage abs1.png abs_tsne.png abs_gradients.png -title 'Absolute value' -pointsize 30 -tile 3x1 -geometry +2+2 abs_montage.png
+#Add labels
+convert overlap1.png -pointsize 50 -gravity NorthWest -annotate +0+0 "H" overlap1.png
+convert overlap_tsne.png -pointsize 50 -gravity NorthWest -annotate +0+0 "I" overlap_tsne.png
+convert overlap_gradients.png -pointsize 50 -gravity NorthWest -annotate +0+0 "J" overlap_gradients.png
+montage overlap1.png overlap_tsne.png overlap_gradients.png -title 'Overlap' -pointsize 30 -tile 3x1 -geometry +2+2 overlap_montage.png
+#All
+montage FC_montage.png abs_montage.png overlap_montage.png -tile 1x3 -geometry +2+2 all_montage.png
